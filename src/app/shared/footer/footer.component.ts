e807a5b716eb66db93883045416b3eb10403da2d
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2019-10-12 01:16:34 +0100

    Footer done, except mobile view

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faEnvelope, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+  //FontAwesome icons
+  faEnvelope = faEnvelope;
+  faPaperPlane = faPaperPlane;
 
   constructor() { }
 
