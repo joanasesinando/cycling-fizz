240e4d65875622a3e7d230fbe8eadcd0bd939634
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2019-10-29 21:35:38 +0000

    AOS: animate on scroll dependency added & used in features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
-import * as $ from 'jquery';
+import {Component, OnInit} from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'cycling-fizz';
+
+  ngOnInit() {
+    AOS.init();
+  }
 }
