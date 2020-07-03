afbce24f0d390d696fd5ecca583098e44d496c1e
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2020-07-03 15:55:53 +0100

    CREDITS: updated

--- src/app/shared/credits/credits-accordion/credits-accordion.component.ts
+++ src/app/shared/credits/credits-accordion/credits-accordion.component.ts
@@ -12,6 +12,7 @@ export class CreditsAccordionComponent implements OnInit {
   homepage: {img: string, by: string, byUrl: string, from: string, fromUrl: string}[] = creditsInfo.homepage;
   map: {img: string, by: string, byUrl: string, from: string, fromUrl: string}[] = creditsInfo.map;
   the_project: {img: string, by: string, byUrl: string, from: string, fromUrl: string}[] = creditsInfo.theProject;
+  contact: {img: string, by: string, byUrl: string, from: string, fromUrl: string}[] = creditsInfo.contact;
 
   login: {img: string, by: string, byUrl: string, from: string, fromUrl: string}[] = creditsInfo.login;
   navbar: {img: string, by: string, byUrl: string, from: string, fromUrl: string}[] = creditsInfo.navbar;
@@ -25,6 +26,7 @@ export class CreditsAccordionComponent implements OnInit {
       ['navbar.homepage', this.homepage],
       ['navbar.map', this.map],
       ['navbar.about.dropdown.project', this.the_project],
+      ['navbar.about.dropdown.contact', this.contact],
       ['Login', this.login],
       ['Navbar', this.navbar],
     ];
